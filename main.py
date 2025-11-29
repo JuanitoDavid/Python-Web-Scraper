6c4318a9506531bde1a74951407b57fef561c062
Roniel David Castro <rocastron@unal.edu.co>
2025-11-29 16:16:20 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+if __name__ == "__main__": #marcador de posición
+    scraper = WebScraper("https://tambinsoyunal.fandom.com/es/wiki/Universidad_Nacional_de_Colombia")
+    titulos = scraper.extract_titles()
+    parrafos = scraper.extract_paragraphs()
+
+    print("=== TÍTULOS ===")
+    for t in titulos:
+        print("-", t)
+
+    print("\n=== PÁRRAFOS (primeros 5) ===")
+    for p in parrafos[:5]:
+        print(p, "\n")
